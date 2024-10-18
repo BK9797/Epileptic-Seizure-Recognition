12e64d5aa2e0d3f0f9600beec0867ac9c68b815f
Revald1o <139723745+BK9797@users.noreply.github.com>
2024-10-18 20:56:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,46 +6,36 @@ from tensorflow.keras.layers import Dense, BatchNormalization, Dropout
 # Specify which 10 features you want to collect
 input_indices = [1, 15, 30, 45, 60, 75, 90, 105, 120, 135]
 
-import pickle
-import numpy as np
-import pandas as pd
+@st.cache_resource
+def create_model():
+    # Build the ANN model for binary classification
+    model = Sequential()
 
-# Load the trained model
-with open(
-    r"W:/Users/bishoykamel/Documents/Study/Git/Epileptic-Seizure-Recognition/Epileptic-Seizure-Recognition/model.pkl", "rb"
-) as f:
-    model = pickle.load(f)
-    
-# @st.cache_resource
-# def create_model():
-#     # Build the ANN model for binary classification
-#     model = Sequential()
+    # Input layer
+    model.add(Dense(256, input_shape=(len(input_indices),), activation='elu'))
+    model.add(BatchNormalization())  # Batch Normalization after input layer
+    model.add(Dropout(0.2))  # Dropout after Batch Normalization
 
-#     # Input layer
-#     model.add(Dense(256, input_shape=(len(input_indices),), activation='elu'))
-#     model.add(BatchNormalization())  # Batch Normalization after input layer
-#     model.add(Dropout(0.2))  # Dropout after Batch Normalization
+    # Hidden layers
+    model.add(Dense(128, activation='elu'))
+    model.add(Dense(64, activation='elu'))
+    model.add(BatchNormalization())  # Batch Normalization after first hidden layer
+    model.add(Dropout(0.3))  # Dropout after Batch Normalization
 
-#     # Hidden layers
-#     model.add(Dense(128, activation='elu'))
-#     model.add(Dense(64, activation='elu'))
-#     model.add(BatchNormalization())  # Batch Normalization after first hidden layer
-#     model.add(Dropout(0.3))  # Dropout after Batch Normalization
+    model.add(Dense(32, activation='elu'))
+    model.add(BatchNormalization())  # Batch Normalization after second hidden layer
+    model.add(Dropout(0.2))  # Dropout after Batch Normalization
 
-#     model.add(Dense(32, activation='elu'))
-#     model.add(BatchNormalization())  # Batch Normalization after second hidden layer
-#     model.add(Dropout(0.2))  # Dropout after Batch Normalization
+    # Output layer (for binary classification)
+    model.add(Dense(1, activation='sigmoid'))  # Output is 1 neuron with sigmoid for binary classification
 
-#     # Output layer (for binary classification)
-#     model.add(Dense(1, activation='sigmoid'))  # Output is 1 neuron with sigmoid for binary classification
+    # Compile the model
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-#     # Compile the model
-#     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
 
-#     return model
-
-# # Create the model
-# model = create_model()
+# Create the model
+model = create_model()
 
 # Title and description
 st.title("Epileptic Seizure Recognition")
